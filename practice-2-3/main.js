be1e841d05e774ae3fad377603c230f2d751dafa
ConanWZP <yaroslav2281337@mail.ru>
2023-09-21 20:07:48 +0300

    tasks

---
+++ practice-2-3/main.js
@@ -0,0 +1,145 @@
+import './style.css'
+import './main.scss'
+
+import { setupCounter } from './counter.js'
+
+const array = []
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <input id="searchValue" />
+    <button id="fetch">Загрузить данные</button>
+    <table class='table'>
+                        <thead class='thead'>
+                            <tr class='tr'>
+                                <th class='th' id="userID-switcher">User ID</th>
+                                <th class='th' id="id-switcher">ID</th>
+                                <th class='th' id="title-switcher">Заголовок</th>
+                                <th class='th' id="description-switcher">Описание</th>
+                            </tr>
+                        </thead>
+                        <tbody id="tbody">
+                        
+</tbody>
+                        
+                       
+                    </table>
+  </div>
+`
+
+//setupCounter(document.querySelector('#counter'))
+
+const fetchButton = document.getElementById('fetch')
+
+const dataArray = []
+
+if (fetchButton) {
+    fetchButton.addEventListener('click', () => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(response => response.json())
+            .then(json => {
+                console.log(json)
+                dataArray.push(...json)
+                render(dataArray)
+            })
+
+
+    })
+}
+
+const render = (array) => {
+    return document.getElementById('tbody').innerHTML = array.map((post, index) =>
+        `
+       <tr class='${index %2 ? "odd": ""}'>
+            <td class='td'>${post.userId}</td>
+            <td class='td'>${post.id}</td>
+            <td class='td'>${post.title}</td>
+            <td class='td'>${post.body}</td>
+        </tr>
+`).join('')
+}
+
+
+
+const userIDSwitcher = document.getElementById('userID-switcher')
+const idSwitcher = document.getElementById('id-switcher')
+const titleSwitcher = document.getElementById('title-switcher')
+const descriptionSwitcher = document.getElementById('description-switcher')
+let filteredData = dataArray
+
+
+
+let isSortedByUserId = true
+userIDSwitcher.addEventListener('click', () => {
+    if (isSortedByUserId) {
+        filteredData.sort((a, b) => a.userId - b.userId)
+        isSortedByUserId = false
+    } else {
+        filteredData.sort((a, b) => b.userId - a.userId)
+        isSortedByUserId = true
+    }
+    render(filteredData)
+})
+
+let isSortedById = true
+idSwitcher.addEventListener('click', () => {
+    if (isSortedById) {
+        filteredData.sort((a, b) => a.id - b.id)
+        isSortedById = false
+    } else {
+        filteredData.sort((a, b) => b.id - a.id)
+        isSortedById = true
+    }
+    render(filteredData)
+})
+
+
+let isSortedByTitle = true
+titleSwitcher.addEventListener('click', () => {
+    console.log('Клик')
+    if (isSortedByTitle) {
+        filteredData.sort((a, b) => a.title.localeCompare(b.title))
+        isSortedByTitle = false
+    } else {
+        filteredData.sort((a, b) => b.title.localeCompare(a.title))
+        isSortedByTitle = true
+    }
+    render(filteredData)
+})
+
+let isSortedByDescription = false
+descriptionSwitcher.addEventListener('click', () => {
+    if (isSortedByDescription) {
+        filteredData.sort((a, b) => a.body.localeCompare(b.body))
+        isSortedByDescription = false
+    } else {
+        filteredData.sort((a, b) => b.body.localeCompare(a.body))
+        isSortedByDescription = true
+    }
+    render(filteredData)
+})
+
+const searchValue = document.getElementById('searchValue')
+
+
+
+
+searchValue.addEventListener('keyup', () => {
+   // console.log('Жмяк')
+    console.log(searchValue.value)
+    if (searchValue.value.length >=3) {
+
+        console.log(searchValue.value)
+        console.log('Попал сюда')
+        filteredData = dataArray.filter((obj) => obj.title.toLowerCase().includes(searchValue.value.toLowerCase() || obj.body.toLowerCase().includes(searchValue.value.toLowerCase())))
+        /*dataArray.filter((obj) => obj.title.toLowerCase().includes(searchValue.value.toLowerCase() || obj.body.toLowerCase().includes(searchValue.value.toLowerCase())))
+        console.log(dataArray)*/
+        render(filteredData)
+    } else {
+        filteredData = dataArray
+        render(dataArray)
+    }
+
+
+
+})
---
+++ project-1/main.js
@@ -0,0 +1,163 @@
+import './style.css'
+import './app.scss'
+
+
+
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <div class="card">
+        <button id="open" class="open-btn" type="button">Open modal</button>
+    </div>
+    <dialog class="modal-container">
+        <div class="modal-content">
+            <div class="modal-title">Стать партнёром проекта</div>
+            <form action="/" method="GET" id="form" class="modal-main__info">
+                <div class="modal-main__top">
+                    <div class="modal-main__left">
+                        <div class="modal-main__input-wrapper">
+                            <div class="modal-main__sign"><span>✱</span><span>Название организации</span></div>
+                            <input class="modal-main__input" name="org-name" id="org-name" required/>
+                        </div>
+                        <div class="modal-main__input-wrapper">
+                            <div class="modal-main__sign"><span>✱</span><span>Телефон</span></div>
+                            <input class="modal-main__input" name="phone" id="phone" />
+                        </div>
+                        <div class="modal-main__input-wrapper">
+                            <div class="modal-main__sign"><span>✱</span><span>E-mail</span></div>
+                            <input class="modal-main__input" required name="email" type="email" id="email" />
+                        </div>
+                    </div>
+                    <div class="modal-main__right">
+                        <div class="modal-main__sign"><span>✱</span><span>Логотип (jpeg, png)</span></div>
+                        <div class="modal-logo">
+                             <img src="./assets/Ellipse%2044.svg" />
+                             <img class="modal-logo__close" src="./assets/Group%202394.svg" />
+                             <div class="modal-logo__file">
+                                <img src="./assets/Vector%20(4).svg" />
+                                <div>Выберите файл</div>
+                                <input id="file" type="file" name="file" accept='image/*' />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-main__center">
+                    <div class="modal-main__input-wrapper modal-main__large modal-main__select">
+                            <div class="modal-main__sign"><span>✱</span><span>Направление</span></div>
+                            <!--<input class="modal-main__input" value="+7 933 848-34-33" />-->
+                            <select class="modal-main__input modal-select" required name="direction" id="direction">
+                                  <option value="" disabled selected>--Выберите значение --</option>
+                                  <option value="eco">Экология</option>
+                                  <option value="math">Математика</option>
+                            </select>
+                            <img id="select" src="./assets/Group.svg" />
+                    </div>
+                    <div class="modal-main__input-wrapper modal-main__large">
+                            <input class="modal-main__input"  />
+                            <img id="select" src="./assets/Group.svg" />
+                    </div>
+                    <div class="modal-main__input-wrapper modal-main__large">
+                            <input class="modal-main__input"  />
+                            <img id="select" src="./assets/global%201.svg" />
+                    </div>
+                    <div class="modal-main__input-wrapper modal-main__large">
+                            <input class="modal-main__input"  />
+                            <img id="select" src="./assets/vk%201.svg" />
+                    </div>
+                    <div class="modal-main__input-wrapper modal-main__large">
+                            <input class="modal-main__input"  />
+                            <img id="select" src="./assets/Icon.svg" />
+                    </div>
+                    <div class="modal-main__input-wrapper modal-main__large">
+                            <input class="modal-main__input" />
+                            <img id="select" src="./assets/Instagram_Color_icon-icons.com_71811%201.svg" />
+                    </div>
+                    <div class="modal-main__input-wrapper modal-main__large">
+                            <input class="modal-main__input"  />
+                            <img id="select" src="./assets/youtube%202.svg" />
+                    </div>
+                    <div class="modal-main__input-wrapper modal-main__large modal-main__select">
+                            <div class="modal-main__sign"><span></span><span>Руководитель</span></div>
+                            <input class="modal-main__input"  />
+                    </div>
+                </div>
+                <div id="error" class="modal-error"></div>
+                
+                <button type="submit" class="modal-send">Стать партнёром проекта</button>
+            <button type="button" class="modal-close">Отменить</button>
+            </form>
+        </div>
+        
+    </dialog>
+    <div id="success" class="modal-success"></div>
+  </div>
+`
+
+
+
+const modal = document.querySelector('.modal-container')
+const openModal = document.querySelector('#open')
+const closeModal = document.querySelector('.modal-close')
+
+
+openModal.addEventListener('click', () => {
+    modal.showModal()
+})
+
+closeModal.addEventListener('click', () => {
+    modal.close()
+})
+
+const divFile = document.querySelector('.modal-logo__file')
+const fileInput = document.querySelector('#file')
+
+divFile.addEventListener('click', () => {
+    fileInput.click()
+})
+
+
+const form = document.getElementById('form')
+const orgName = document.getElementById('org-name')
+const phone = document.getElementById('phone')
+const direction = document.getElementById('direction')
+const email = document.getElementById('email')
+const errorElement = document.getElementById('error')
+const successElement = document.getElementById('success')
+
+form.addEventListener('submit', (e) => {
+    let messages = []
+    console.log(fileInput.value)
+    if (orgName.value.length < 2) {
+        messages.push('Название должно быть длиннее двух символов')
+    }
+
+    let regPhone = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im
+    let regEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+    if (!regPhone.test(phone.value)) {
+        e.preventDefault()
+        messages.push('Неверно указан формат номера')
+    }
+
+    if (!regEmail.test(email.value)) {
+        e.preventDefault()
+        messages.push('Неверно указан формат почты')
+    }
+
+    if (!fileInput.value) {
+        messages.push('Добавьте логотип')
+    }
+
+    if (messages.length > 0) {
+        e.preventDefault()
+        errorElement.innerText = messages.join(', ')
+    } else {
+        messages = []
+        e.preventDefault()
+        successElement.innerText = 'Форма отправлена'
+        form.reset()
+        modal.close()
+    }
+
+
+})
